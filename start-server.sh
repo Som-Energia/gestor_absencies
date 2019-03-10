892471408e10fb83cc45b4c67d53ad18a42ad509
BenjiRS94 <benjami94@gmail.com>
2019-03-10 23:27:48 +0100

    Improved docker-complose && Dockerfile

---
+++ start-server.sh
@@ -0,0 +1,7 @@
+pip install ipython
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+python manage.py migrate --settings=config.settings.develop
+python manage.py runserver 0.0.0.0:8000 --settings=config.settings.develop
