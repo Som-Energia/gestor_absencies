5f57a4a33b01e01e6a68081d419730127727ea07
Benjami Ramos Solis <benjami.ramos@somenergia.coop>
2019-03-20 08:17:03 +0100

    Updated django migrations

--- start-server.sh
+++ start-server.sh
@@ -2,6 +2,6 @@ pip install ipython
 
 pip install -r requirements.txt
 
-python manage.py makemigrations
+python manage.py makemigrations gestor_absencies
 python manage.py migrate --settings=config.settings.develop
 python manage.py runserver 0.0.0.0:8000 --settings=config.settings.develop
