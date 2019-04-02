ba88c21582e547c519e37314a2ca018744c3d569
Benjami Ramos Solis <benjami.ramos@somenergia.coop>
2019-04-02 09:53:27 +0200

    Added create and calculate dates helpers methods

--- gestor_absencies/tests/test_helper.py
+++ gestor_absencies/tests/test_helper.py
@@ -1,4 +1,13 @@
-from gestor_absencies.models import Worker, Team, Member
+from gestor_absencies.models import (
+    Worker,
+    Team,
+    Member,
+    VacationPolicy,
+    SomEnergiaAbsenceType,
+    SomEnergiaOccurrence
+)
+import dateutil
+from datetime import timedelta
 
 
 worker_attributes = {
@@ -39,3 +48,59 @@ def create_member(worker, team):
     )
     member.save()
     return member
+
+
+def create_vacationpolicy(description, name='normal', holidays=25):
+    vacationpolicy = VacationPolicy(
+        name=name,
+        description=description,
+        holidays=holidays
+    )
+    vacationpolicy.save()
+    return vacationpolicy
+
+
+def create_absencetype(abbr, label, spend_days, min_duration, max_duration):
+    absencetype = SomEnergiaAbsenceType(
+        abbr=abbr,
+        label=label,
+        spend_days=spend_days,
+        min_duration=min_duration,
+        max_duration=max_duration,
+    )
+    absencetype.save()
+    return absencetype
+
+
+def create_occurrence(absence, start_time, end_time):
+    occurrence = SomEnergiaOccurrence(
+        absence=absence,
+        start_time=start_time,
+        end_time=end_time
+    )
+    occurrence.save()
+    return occurrence
+
+
+def days_between_dates(start_time, end_time, dates_types):
+    return len(list(dateutil.rrule.rrule(
+        dtstart=start_time,
+        until=end_time,
+        freq=dateutil.rrule.DAILY,
+        byweekday=dates_types
+    )))
+
+
+def calculate_occurrence_dates(start_time, duration, spend_days):
+    workday = [0, 1, 2, 3, 4]
+    weekend = [5, 6]
+    end_time = start_time
+
+    if spend_days < 0 or spend_days == 0:
+        while days_between_dates(start_time, end_time, workday) < duration:
+            end_time = end_time + timedelta(days=1)
+    elif spend_days > 0:
+        while days_between_dates(start_time, end_time, weekend) < duration:
+            end_time = end_time + timedelta(days=1)
+
+    return end_time
--- gestor_absencies/tests/test_occurrences.py
+++ gestor_absencies/tests/test_occurrences.py
@@ -1,122 +1,139 @@
 from django.test import TestCase
 from gestor_absencies.models import (
-	Worker,
-	SomEnergiaAbsenceType,
-	SomEnergiaAbsence,
-	SomEnergiaOccurrence,
-	VacationPolicy
+    Worker,
+    SomEnergiaAbsenceType,
+    SomEnergiaAbsence,
+    SomEnergiaOccurrence,
+    VacationPolicy
 )
 from django.urls import reverse
 from datetime import (
-	datetime as dt,
-	timedelta as td
+    datetime as dt,
+    timedelta as td
+)
+from gestor_absencies.tests.test_helper import (
+    calculate_occurrence_dates,
+    create_worker,
+    create_vacationpolicy,
+    create_absencetype,
+    create_occurrence
 )
 
+
 class SomEnergiaOccurrenceTest(TestCase):
-	def setUp(self):
-		self.base_url = reverse('absences')
+    def setUp(self):
+        self.base_url = reverse('absences')
 
-		self.test_vacationpolicy = VacationPolicy(
-            name='normal',
-            description='normal vacation policy',
-            holidays=25
+        self.test_vacationpolicy = create_vacationpolicy(
+            description='normal vacation policy'
         )
-		self.test_vacationpolicy.save()
 
-		self.test_admin = Worker(
-			first_name='admin',
-            last_name='admin',
-            email='admin@admin.com',
-            username='admin',
-            password='admin'
+        self.test_admin = create_worker(username='admin', is_admin=True)
+
+        self.test_admin.vacation_policy = self.test_vacationpolicy
+        self.test_admin.save()
+
+        self.test_absencetype = create_absencetype(
+            abbr='bdef',
+            label='Baixa defuncio',
+            spend_days=0,
+            min_duration=3,
+            max_duration=3,
         )
-		self.test_admin.set_password('admin')
-		self.test_admin.is_superuser = True
-		self.test_admin.vacation_policy = self.test_vacationpolicy
-		self.test_admin.save()
+        self.id_absencetype = self.test_absencetype.pk
 
-		self.test_absencetype = SomEnergiaAbsenceType(
-		    abbr='bdef',
-		    label='Baixa defuncio',
-		    spend_days=0,
-		    min_duration=3,
-		    max_duration=3,
-		)
-		self.test_absencetype.save()
-		self.id_absencetype = self.test_absencetype.pk
+        self.test_absence = self.test_admin.somenergiaabsence_set.all()[0]
+        self.id_absence = self.test_absence.pk
 
-		self.test_absence = self.test_admin.somenergiaabsence_set.all()[0]
-		self.id_absence = self.test_absence.pk
+        self.testoccurrence_start_time = (dt.now() + td(days=1)).replace(microsecond=0)
+        self.test_occurrence = create_occurrence(
+            absence=self.test_absence,
+            start_time=self.testoccurrence_start_time,
+            end_time=calculate_occurrence_dates(
+                self.testoccurrence_start_time, 3, 0
+            ),
+        )
+        self.id_occurrence = self.test_occurrence.pk
 
-		self.test_occurrence = SomEnergiaOccurrence(
-			absence=self.test_absence,
-			start_time=(dt.now() + td(days=1)).replace(microsecond=0),
-			end_time=(dt.now() + td(days=4)).replace(microsecond=0),
-		)
-		self.test_occurrence.save()
-		self.id_occurrence = self.test_occurrence.pk
+    def test__list_occurrences(self):
+        self.client.login(username='admin', password='password')
+        response = self.client.get(
+            self.base_url
+        )
 
-	def test__list_occurrences(self):
-		self.client.login(username='admin', password='admin')
-		response = self.client.get(
-			self.base_url
-		)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['count'], 1)
+        self.assertEqual(response.json()['next'], None)
+        self.assertEqual(response.json()['previous'], None)
+        self.assertEqual(
+            response.json()['results'][0]['absence'], self.id_absence
+        )
+        self.assertEqual(
+            response.json()['results'][0]['start_time'],
+            '{0:%Y-%m-%dT%H:%M:%SZ}'.format(self.testoccurrence_start_time)
+        )
+        self.assertEqual(
+            response.json()['results'][0]['end_time'],
+            '{0:%Y-%m-%dT%H:%M:%SZ}'.format(
+                calculate_occurrence_dates(self.testoccurrence_start_time, 3, 0)
+            )
+        )
 
-		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.json()['count'], 1)
-		self.assertEqual(response.json()['next'], None)
-		self.assertEqual(response.json()['previous'], None)
-		self.assertEqual(response.json()['results'][0]['absence'], self.id_absence)
-		self.assertEqual(response.json()['results'][0]['start_time'], '{0:%Y-%m-%dT%H:%M:%SZ}'.format(dt.now() + td(days=1)))
-		self.assertEqual(response.json()['results'][0]['end_time'], '{0:%Y-%m-%dT%H:%M:%SZ}'.format(dt.now() + td(days=4)))
+    def test__simple_post_occurrence(self): # Todo: more cases
+        start_time = (dt.now() + td(days=3)).replace(microsecond=0)
+        body = {
+            'absence': self.id_absence,
+            'start_time': start_time,
+            'start_morning': True,
+            'start_afternoon': True,
+            'end_time': calculate_occurrence_dates(start_time, 3, 0),
+            'end_morning': True,
+            'end_afternoon': True
+        }
+        self.client.login(username='admin', password='password')
+        response = self.client.post(
+            self.base_url, data=body
+        )
 
-	def test__simple_post_occurrence(self): # Todo: more cases
-		body = {
-			'absence': self.id_absence,
-			'start_time': (dt.now() + td(days=1)).replace(microsecond=0),
-			'start_morning': True,
-			'start_afternoon': True,
-			'end_time': (dt.now() + td(days=4)).replace(microsecond=0),
-			'end_morning': True,
-			'end_afternoon': True
-		}
-		self.client.login(username='admin', password='admin')
-		response = self.client.post(
-			self.base_url, data=body
-		)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['absence'], self.id_absence)
+        self.assertEqual(
+            response.json()['start_time'],
+            '{0:%Y-%m-%dT%H:%M:%SZ}'.format(
+                (start_time).replace(hour=9, minute=0, second=0))
+        )
+        self.assertEqual(
+            response.json()['end_time'],
+            '{0:%Y-%m-%dT%H:%M:%SZ}'.format(
+                (calculate_occurrence_dates(start_time, 3, 0)).replace(
+                    hour=17,
+                    minute=0,
+                    second=0
+                )
+            )
+        )
 
-		expected = {
-			'id': 2,
-			'absence': self.id_absence,
-			'start_time': dt.now() + td(days=1),
-			'end_time': (dt.now() + td(days=4)),	
-		}
-		self.assertEqual(response.status_code, 201)
-		self.assertEqual(response.json()['absence'], self.id_absence)
-		self.assertEqual(response.json()['start_time'], '{0:%Y-%m-%dT%H:%M:%SZ}'.format((dt.now() + td(days=1)).replace(hour=9, minute=0, second=0)))
-		self.assertEqual(response.json()['end_time'], '{0:%Y-%m-%dT%H:%M:%SZ}'.format((dt.now() + td(days=4)).replace(hour=17, minute=0, second=0)))
+    def test__delete_occurrence(self):
+        self.client.login(username='admin', password='password')
+        response = self.client.delete(
+            '/'.join([self.base_url, str(self.id_occurrence)])
+        )
+        self.assertEqual(response.status_code, 204)
 
-	def test__delete_occurrence(self):
-		self.client.login(username='admin', password='admin')
-		response = self.client.delete(
-			'/'.join([self.base_url, str(self.id_occurrence)])
-		)
-		self.assertEqual(response.status_code, 204)
+    def test__cant_delete_started_occurrence(self):
+        start_time = (dt.now() + td(days=-1)).replace(microsecond=0)
+        self.new_occurrence = create_occurrence(
+            absence=self.test_absence,
+            start_time=start_time,
+            end_time=calculate_occurrence_dates(start_time, 3, 0)
+        )
 
-	def test__cant_delete_started_occurrence(self):
-		self.new_occurrence = SomEnergiaOccurrence(
-			absence=self.test_absence,
-			start_time=(dt.now() + td(days=-1)).replace(microsecond=0),
-			end_time=(dt.now() + td(days=3)).replace(microsecond=0),	#Todo: create a helper method to calculate correct date
-		)
-		self.new_occurrence.save()
-
-		self.client.login(username='admin', password='admin')
-		response = self.client.delete(
-			'/'.join([self.base_url, str(self.new_occurrence.pk)])
-		)
-		self.assertEqual(response.status_code, 400)
-		self.assertEqual(response.json()[0], 'Can not delete')
+        self.client.login(username='admin', password='password')
+        response = self.client.delete(
+            '/'.join([self.base_url, str(self.new_occurrence.pk)])
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()[0], 'Can not delete')
 
 
 # TODO:
@@ -136,3 +153,6 @@ class SomEnergiaOccurrenceTest(TestCase):
 # test delete other worker occurrence's (admin)
 # test delete with spend_days +1 substraction holidays
 # test delete with spend_days -1 generate holidays
+
+
+# duration != compute days
