072fc64416aa466ecdbdc4a87218ba5dfe75c461
Benjami <benjami94@gmail.com>
2020-04-15 10:11:39 +0200

    Add change_year test with occurrences

--- gestor_absencies/scheduler_tasks.py
+++ gestor_absencies/scheduler_tasks.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime as dt
 
 from .models import SomEnergiaOccurrence, Worker
 from django.db import transaction
@@ -8,10 +9,18 @@ logger = logging.getLogger('scheduler_tasks')
 
 @transaction.atomic
 def change_year():
+    now = dt.now()
     logger.info("Start change_year")
     workers = Worker.objects.all()
     for worker in workers:
         logger.debug("Update {} holidays", worker.email)
         worker.holidays += worker.vacation_policy.holidays
+        next_year_occurrences = SomEnergiaOccurrence.objects.filter(
+            start_time__year=now.year + 1,
+            end_time__year=now.year + 1,
+            absence__worker=worker
+        )
+        for next_year_occurrence in next_year_occurrences:
+            worker.holidays += next_year_occurrence.day_counter()
         worker.save()
     logger.info("change_year finished")
--- gestor_absencies/tests/test_schedulertasks.py
+++ gestor_absencies/tests/test_schedulertasks.py
@@ -1,19 +1,25 @@
 import logging
+from datetime import timedelta as td, datetime as dt
 from django.test import TestCase
 from testfixtures import LogCapture
-from .test_helper import (create_worker, create_vacationpolicy)
+from .test_helper import (create_worker, create_vacationpolicy,
+                          create_absencetype, create_occurrence,
+                          calculate_occurrence_dates)
 from gestor_absencies.models import (SomEnergiaOccurrence, Worker)
 from gestor_absencies.scheduler_tasks import change_year
+
 
 class SchedulerTasksTest(TestCase):
 
     def setUp(self):
-        self.worker = create_worker(username='Worker', email='worker@mail.coop')
-        vacation_policy = create_vacationpolicy(
+        self.worker = create_worker(
+            username='Worker', email='worker@mail.coop'
+        )
+        self.vacation_policy = create_vacationpolicy(
             description='normal vacation policy',
             created_by=self.worker
         )
-        self.worker.vacation_policy = vacation_policy
+        self.worker.vacation_policy = self.vacation_policy
         self.worker.holidays = 10
         self.worker.save()
 
@@ -21,7 +27,19 @@ class SchedulerTasksTest(TestCase):
         self.worker.delete()
         LogCapture.uninstall_all()
 
-    def test__change_year__update_worker_holidays(self):
+    def setup_absencetypes(self):
+        self.holiday_absence_type = create_absencetype(
+            name='Vacances', description='vacances',
+            spend_days=-1, min_duration=0.5, max_duration=-1,
+            created_by=self.worker, color='#fcba03'
+        )
+        self.school_absence_type = create_absencetype(
+            name='Escola', description='escola',
+            spend_days=1, min_duration=0.5, max_duration=-1,
+            created_by=self.worker, color='#fcba03'
+        )
+
+    def test__change_year__update_worker_holidays_without_occurrences(self):
         past_holidays = self.worker.holidays
 
         with LogCapture(level=logging.INFO) as log_captured:
@@ -36,4 +54,28 @@ class SchedulerTasksTest(TestCase):
         self.assertListEqual(
             log_messages,
             ["Start change_year", "change_year finished"]
+        )
+
+    def test__change_year__update_worker_holidays_with_occurrences(self):
+        past_holidays = self.worker.holidays
+        self.setup_absencetypes()
+        start_time = dt(dt.now().year + 1, 2, 5)
+        create_occurrence(
+            absence_type=self.holiday_absence_type, worker=self.worker,
+            start_time=start_time,
+            end_time=calculate_occurrence_dates(start_time, 10, -1)
+        )
+        start_time = dt(dt.now().year + 1, 3, 5)
+        create_occurrence(
+            absence_type=self.school_absence_type, worker=self.worker,
+            start_time=start_time,
+            end_time=calculate_occurrence_dates(start_time, 2, 1)
+        )
+
+        change_year()
+        self.worker.refresh_from_db()
+
+        self.assertEqual(
+            self.worker.holidays,
+            past_holidays + self.worker.vacation_policy.holidays - 10 + 2
         )
